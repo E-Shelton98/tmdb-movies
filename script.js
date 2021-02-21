224f3b62245d27e96ef648d8532cafa083975870
Erik Shelton <erikshelton@Eriks-Air.lan1>
2021-02-21 05:16:06 -0500

    fetching from TMDb through backend server

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+const FORM = document.getElementById('form')
+let searchBar = document.getElementById('search')
+let mediaType = 'tv'
+
+
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+
+async function getMovies(route, mediaType, searchedItem) {
+  const DISCOVER = `https://tmdb-movies-backend.herokuapp.com/discover?type=${mediaType}`
+  const SEARCH = `https://tmdb-movies-backend.herokuapp.com/search?type=${mediaType}&query=${searchedItem}`
+
+ 
+  if (route === 'search') {
+  const res = await fetch(SEARCH)
+  let data = await res.json()
+
+  console.log(data.data.results)
+  } else if (route === 'discover') {
+    const res = await fetch(DISCOVER)
+    let data = await res.json()
+
+    console.log(data.data.results)
+  }
+
+  
+}
+
+FORM.addEventListener('submit', (e) => {
+  e.preventDefault()
+  let searchTerm = searchBar.value
+  if (searchTerm && searchBar !== '') {
+    getMovies('search', mediaType, searchTerm)
+
+    searchTerm.value = ''
+  }
+})
