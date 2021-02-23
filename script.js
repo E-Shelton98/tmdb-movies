5699e659a154b30a912de0129a3955c267297eec
Erik Shelton <erikshelton@Eriks-Air.lan1>
2021-02-22 20:54:57 -0500

    looping through movies

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
+const MAIN = document.getElementById('main')
 const FORM = document.getElementById('form')
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 let searchBar = document.getElementById('search')
 let mediaType = 'tv'
 
-
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+getMovies('discover', 'movie')
 
 async function getMovies(route, mediaType, searchedItem) {
   const DISCOVER = `https://tmdb-movies-backend.herokuapp.com/discover?type=${mediaType}`
   const SEARCH = `https://tmdb-movies-backend.herokuapp.com/search?type=${mediaType}&query=${searchedItem}`
 
- 
   if (route === 'search') {
-  const res = await fetch(SEARCH)
-  let data = await res.json()
+    const res = await fetch(SEARCH)
+    let data = await res.json()
 
-  console.log(data.data.results)
+    console.log(data.data.results)
   } else if (route === 'discover') {
     const res = await fetch(DISCOVER)
     let data = await res.json()
 
     console.log(data.data.results)
   }
-
-  
 }
 
 FORM.addEventListener('submit', (e) => {
@@ -34,3 +32,11 @@ FORM.addEventListener('submit', (e) => {
     searchTerm.value = ''
   }
 })
+
+function showMovies(movies) {
+  MAIN.innerHTML = ''
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie
+  })
+}
