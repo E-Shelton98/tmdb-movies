9323ddc0527ef8f6fae30b2f92a15ddaf058bb29
Erik Shelton <erikshelton@Eriks-Air.lan1>
2021-02-23 22:47:03 -0500

    movies load on page load

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ async function getMovies(route, mediaType, searchedItem) {
     const res = await fetch(SEARCH)
     let data = await res.json()
 
-    console.log(data.data.results)
+    showMovies(data.data.results)
   } else if (route === 'discover') {
     const res = await fetch(DISCOVER)
     let data = await res.json()
 
-    console.log(data.data.results)
+    showMovies(data.data.results)
   }
 }
 
@@ -38,5 +38,36 @@ function showMovies(movies) {
 
   movies.forEach((movie) => {
     const { title, poster_path, vote_average, overview } = movie
+
+    const movieEl = document.createElement('div')
+
+    movieEl.classList.add('movie')
+
+    movieEl.innerHTML = `
+        <img
+          src="${IMG_PATH + poster_path}"
+          alt="${title}"
+        />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+      `
+
+    main.appendChild(movieEl)
   })
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return 'green'
+  } else if (vote >= 5) {
+    return 'orange'
+  } else {
+    return 'red'
+  }
 }
